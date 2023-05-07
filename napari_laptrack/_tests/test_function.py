d53a5f0ddd3fd12e1782bc517f04ebfc5dfc3ae3
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-05-07 14:40:23 +0200

    skip test because of https://github.com/napari/napari/issues/5810

--- napari_laptrack/_tests/test_function.py
+++ napari_laptrack/_tests/test_function.py
@@ -1,13 +1,15 @@
 from typing import Callable
 
 import napari
+import pytest
+
 
 # from napari_laptrack import threshold, image_arithmetic
 # add your tests here...
 
 PLUGIN_NAME = "napari_laptrack"
 
-
+@pytest.mark.skip(reason="https://github.com/napari/napari/issues/5810")
 def test_sample_data(make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer()
     viewer.open_sample(PLUGIN_NAME, "simple_tracks")
