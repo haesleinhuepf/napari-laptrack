b49c86813e8348fdabd4fa01b82d771eea60a093
Yohsuke Fukai <ysk@yfukai.net>
2023-05-31 23:31:58 +0900

    updated sample and tests

--- napari_laptrack/_tests/test_function.py
+++ napari_laptrack/_tests/test_function.py
@@ -2,7 +2,7 @@ from typing import Callable
 
 import napari
 import pytest
-
+from napari_laptrack._function import _DATA
 
 # from napari_laptrack import threshold, image_arithmetic
 # add your tests here...
@@ -11,4 +11,5 @@ PLUGIN_NAME = "napari_laptrack"
 
 def test_sample_data(make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer(block_plugin_discovery=False)
-    viewer.open_sample(PLUGIN_NAME, "simple_tracks")
+    for k in _DATA.keys():
+        viewer.open_sample(PLUGIN_NAME, k)
