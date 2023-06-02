d39c919699c4f72cf1149143dbe4d1435f032dca
Yohsuke Fukai <ysk@yfukai.net>
2023-06-02 09:22:12 +0900

    trying to update the logic

--- napari_laptrack/_function.py
+++ napari_laptrack/_function.py
@@ -8,6 +8,27 @@ from napari_tools_menu import register_function
 def napari_experimental_provide_function():  # noqa: D103
     return [track_labels_centroid_based]
 
+def _check_and_convert_layers_to_4d(image_layer,labels_layer,viewer):
+    import napari_time_slicer as nts
+    image = image_layer.data
+    labels = labels_layer.data
+    # Deal with input data of various formats and bring both in [T,Z,Y,X] shape/format, maybe with Z=1
+    assert image.shape == labels.shape, "Image and labels must have the same shape."    
+    if len(image) == 2:
+        print("Image data seems 2D/on-the-fly-processed, converting to 3D+t...")
+        image_layer_4d = nts._function.convert_to_stack4d(image_layer, viewer)
+        labels_layer_4d = nts._function.convert_to_stack4d(labels_layer, viewer)
+    elif len(image_layer.data.shape) == 3:
+        print("Image data seems 2D+t, converting to 3D+t...")
+        image_layer_4d = nts._function.convert_to_2d_timelapse(image_layer, viewer)
+        labels_layer_4d = nts._function.convert_to_2d_timelapse(labels_layer, viewer)
+    image_name = image_layer_4d.name
+    viewer.add_image(image, name=image_name)
+    labels_name = labels_layer_4d.name
+    viewer.add_labels(labels, name=labels_name)
+
+    return image_layer_4d, labels_layer_4d
+
 
 @register_function(menu="Tracking > Track labeled objects (centroid-based, LapTrack)")
 def track_labels_centroid_based(
@@ -23,39 +44,13 @@ def track_labels_centroid_based(
     ..[1] https://github.com/yfukai/laptrack/blob/main/docs/examples/bright_spots.ipynb
     """
     from laptrack import LapTrack
+    from laptrack.data_conversion import convert_split_merge_df_to_napari_graph
     import napari_skimage_regionprops as nsr
-    import napari_time_slicer as nts
     import numpy as np
 
-    # Deal with input data of various formats and bring both in [T,Z,Y,X] shape/format, maybe with Z=1
-    image = image_layer.data
-    if len(image_layer.data.shape) == 2:
-        print("Image data seems 2D/on-the-fly-processed, converting to 3D+t...")
-        image_layer_4d = nts._function.convert_to_stack4d(image_layer, viewer)
-        image = image_layer_4d.data
-        image_name = image_layer_4d.name
-        viewer.add_image(image, name=image_name)
-    elif len(image_layer.data.shape) == 3:
-        print("Image data seems 2D+t, converting to 3D+t...")
-        image_layer_4d = nts._function.convert_to_2d_timelapse(image_layer, viewer)
-        image = image_layer_4d.data
-        image_name = image_layer_4d.name
-        viewer.add_image(image, name=image_name)
-
-    labels = labels_layer.data
-    labels_layer_4d = labels_layer
-    if len(labels_layer.data.shape) == 2:
-        print("Labels data seems 2D/on-the-fly-processed, converting to 3D+t...")
-        labels_layer_4d = nts._function.convert_to_stack4d(labels_layer, viewer)
-        labels = labels_layer_4d.data
-        labels_name = labels_layer_4d.name
-        viewer.add_image(image, name=labels_name)
-    elif len(labels_layer.data.shape) == 3:
-        print("Labels data seems 2D+t, converting to 3D+t...")
-        labels_layer_4d = nts._function.convert_to_2d_timelapse(labels_layer, viewer)
-        labels = labels_layer_4d.data
-        labels_name = labels_layer_4d.name
-        viewer.add_image(image, name=labels_name)
+    image_layer_4d, labels_layer_4d = _check_and_convert_layers_to_4d(image_layer,labels_layer,viewer)
+    image = image_layer_4d.data
+    labels = labels_layer_4d.data
 
     # determine centroids
     if (
@@ -81,22 +76,14 @@ def track_labels_centroid_based(
 
     # store results
     labels_layer_4d.features = track_df
-
     # show result as tracks
-    split_merge_graph={}
-    if not split_df.empty:
-        split_merge_graph.update({
-            row["child_track_id"]: [row["parent_track_id"]] for _, row in split_df.iterrows()
-        })
-    if not merge_df.empty:
-        split_merge_graph.update({
-            c_id: grp["parent_track_id"].to_list() for c_id, grp in merge_df.groupby("child_track_id")
-        })
+    split_merge_graph=convert_split_merge_df_to_napari_graph(split_df,merge_df)
     viewer.add_tracks(
-        track_df[["track_id", "frame", "centroid-2", "centroid-0", "centroid-1"]],
+        track_df[["track_id", "frame", "centroid-0", "centroid-1", "centroid-2"]],
         graph=split_merge_graph,
         tail_length=50,
     )
+
     # show result as track-id-label image
     track_id_image = nsr.map_measurements_on_labels(labels_layer_4d, column="track_id")
     track_id_image = track_id_image.astype(np.uint32)
